eda95989224023246d58df7f9f5d7c97790b485f
Jon McCullough <hey@jonm.cc>
2024-04-20 23:38:09 +0200

    More edits to template content

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from 'app/blog/utils'
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://www.jonm.cc'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
