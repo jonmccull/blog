67a029027053c51384091ca29edb77f3492bb37b
Jon McCullough <hey@jonm.cc>
2024-04-20 23:15:54 +0200

    Setup, default content edits

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Jon McCullough
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm Jon, a product marketer working with the positioning and growth of software products.`}
       </p>
       <div className="my-8">
         <BlogPosts />
