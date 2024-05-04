4df29bfe7b54711a57217abc7d9a59d6239bf395
Jon McCullough <hey@jonm.cc>
2024-05-04 07:53:54 +0200

    Revert speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
