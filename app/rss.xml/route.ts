a91568f2a82c06312aa2c5e0a570c0f1be7ffa81
Jon McCullough <hey@jonm.cc>
2025-05-23 00:06:23 +0200

    refactor: consolidate blog posts into content/blog directory

--- app/rss.xml/route.ts
+++ app/rss.xml/route.ts
@@ -1,8 +1,8 @@
 import { baseUrl } from '../sitemap'
-import { getAllPosts } from '../lib/posts'
+import { getAllPosts } from '../lib/blog'
 
 export async function GET() {
-  const posts = await getAllPosts() // You'll need to implement this function
+  const posts = await getAllPosts()
   const feed = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
