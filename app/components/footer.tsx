ee5650d036df633c868818240ffaead4d4b46d61
Jon McCullough <hey@jonm.cc>
2025-05-22 23:21:08 +0200

    Footer layout updates

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,6 +6,7 @@ function ArrowIcon() {
       viewBox="0 0 12 12"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className="inline-block ml-1"
     >
       <path
         d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
@@ -17,32 +18,28 @@ function ArrowIcon() {
 
 export default function Footer() {
   return (
-    <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="/rss"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/jonmccull/blog/"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
-          </a>
-        </li>
-      </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
+    <footer className="mt-12 mb-16 flex flex-col items-start">
+      <div className="flex flex-col md:flex-row gap-4 md:gap-8 text-sm text-neutral-600 dark:text-neutral-400">
+        <a
+          className="group flex items-center hover:text-neutral-800 dark:hover:text-neutral-200 transition-colors"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="/rss"
+        >
+          <span>rss</span>
+          <ArrowIcon />
+        </a>
+        <a
+          className="group flex items-center hover:text-neutral-800 dark:hover:text-neutral-200 transition-colors"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://github.com/jonmccull/blog/"
+        >
+          <span>view source</span>
+          <ArrowIcon />
+        </a>
+      </div>
+      <p className="mt-8 text-sm text-neutral-600 dark:text-neutral-400">
         © {new Date().getFullYear()} Jon McCullough ✌️
       </p>
     </footer>
