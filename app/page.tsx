a78c3f9927313a7e2bfded2ebdb85ccf83214f7b
Jon McCullough <hey@jonm.cc>
2025-05-22 23:13:11 +0200

    Add Todoist link to home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         Jon McCullough
       </h1>
       <p className="mb-4">
-        {`I'm Jon, a marketer specializing in the positioning and growth of software products. Right now I’m a Product Marketer at Doist, working on taking a market-leading todo list app, Todoist, to the next level.`}
+        I'm Jon, a marketer specializing in the positioning and growth of software products. Right now I'm a Product Marketer at Doist, working on taking a market-leading todo list app, <a href="https://www.todoist.com/" className="hover:text-neutral-800 dark:hover:text-neutral-200 transition-colors" target="_blank" rel="noopener noreferrer">Todoist</a>, to the next level.
       </p>
       <p className="mb-4">
         <a href="https://www.linkedin.com/in/jonmccullough/">LinkedIn</a> ⋅ <a href="/cv">CV</a> ⋅ <a href="mailto:hey@jonm.cc">Mail</a> 
